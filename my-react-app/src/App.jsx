594949acbf531083fd92df3ef923d38376142e11
Wilson1009 <143722678+Wilson1009@users.noreply.github.com>
2025-10-03 19:08:45 +0000

    General formatting

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-5xl font-extrabold text-blue-600 p-10">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
