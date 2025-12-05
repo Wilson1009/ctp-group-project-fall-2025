7cd29a338e4508e15686be59596dc25ae32cd1cc
Wilson1009 <wwss1009@gmail.com>
2025-12-05 16:53:23 -0500

    fized what I just messed up and updated README

---
+++ MAPS/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HomePage from "./components/HomePage.jsx"
+import SearchResults from './SearchResults/SearchResults.jsx'
+
+function App() {
+
+  return (
+    <>
+      {/* <HomePage /> */}
+      <SearchResults />
+    </>
+  )
+}
+ 
+export default App
